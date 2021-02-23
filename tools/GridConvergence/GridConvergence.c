dc8419cc94bc769ce958b87e09f2c49c78d77c29
GeorgeVafakos <george.vafakos@gmail.com>
2021-02-23 12:07:07 +0200

    Add tools

--- tools/GridConvergence/GridConvergence.c
+++ tools/GridConvergence/GridConvergence.c
@@ -1,6 +1,28 @@
+/*  ---------------------------------------------------------------------------------------------
+
+    GRID CONVERGENCE
+    ================
+
+    This is a program written in C, that conducts a grid convergence study using the Richardson 
+    Extrapolation. The code isn't dynamic, which means that the eecutable must be compiled after 
+    inserting new data.
+
+---------------------------------------------------------------------------------------------  */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+// Function declaration 
+void allocate_int (int*** arr, int row, int col);
+void allocate_float (float*** arr, int row, int col);
+void allocate_double (double*** arr, int row, int col);
+
+// Perform function overload. If the array to be allocated is int perform the allocate_int function, if it is
+// double perform the allocate_double function. The new function is the allocate function.
+#define allocate(arr, row, col ) _Generic(arr, int***: allocate_int, float***: allocate_float, double***: allocate_double)(arr, row, col)
+
 
 int main()
 {
@@ -11,21 +33,26 @@ int main()
 	
 	// Grid parameters
 	D=2;													// Dimensions of problem
-	SIZE=3;													// Number of test cases
-	Fs=1.25;													// Factor of safety
+	SIZE=5;													// Number of test cases
+	Fs=1.25;												// Factor of safety
 	
-	Nx[0]=168;												// Number of cells in x_direction in the 1st grid
-	Ny[0]=126;												// Number of cells in y_direction in the 1st grid
-	Nx[1]=202;												// Number of cells in x_direction in the 2nd grid
-	Ny[1]=152;												// Number of cells in y_direction in the 2nd grid
-	Nx[2]=242;												// Number of cells in x_direction in the 3rd grid
-	Ny[2]=182;												// Number of cells in y_direction in the 3rd grid
+	Nx[0]=60;												// Number of cells in x_direction in the 1st grid
+	Ny[0]=60;												// Number of cells in y_direction in the 1st grid
+	Nx[1]=72;												// Number of cells in x_direction in the 2nd grid
+	Ny[1]=72;												// Number of cells in y_direction in the 2nd grid
+	Nx[2]=86;												// Number of cells in x_direction in the 3rd grid
+	Ny[2]=86;												// Number of cells in y_direction in the 3rd grid
+	Nx[3]=102;												// Number of cells in x_direction in the 4rd grid
+	Ny[3]=102;												// Number of cells in y_direction in the 4rd grid
+	Nx[4]=122;												// Number of cells in x_direction in the 5rd grid
+	Ny[4]=122;												// Number of cells in y_direction in the 5rd grid
 	
-	
-	
-	f[0]=1.004229;											// The quantity value of the 1st grid
-	f[1]=1.004972;											// The quantity value of the 2nd grid
-	f[2]=1.006468;											// The quantity value of the 3rd grid
+	// Examined quantity in every grid
+	f[0]=-0.564835428785;
+	f[1]=-0.568885281039;
+	f[2]=-0.574592639273;
+	f[3]=-0.57765974803;
+    f[4]=-0.579321656208;
 
 		
 	// Calculate effective refinement ratio
@@ -59,9 +86,9 @@ int main()
 	for (i=0 ; i<SIZE ; i++)
 	{
 		if (i==0 || i==1)
-			printf("%d.\t %dx%d \t \t %.4f \t \t \t \t \t \t \t \t \t \t \t %.2f\n",i+1,Nx[i],Ny[i],f[i],100*fabs(f[SIZE-1]-f[i])/fabs(f[SIZE-1]));
+			printf("%d.\t %dx%d  \t \t %.4f \t \t \t \t \t \t \t \t \t \t \t %.2f\n",i+1,Nx[i],Ny[i],f[i],100*fabs(f[SIZE-1]-f[i])/fabs(f[SIZE-1]));
 		else
-			printf("%d.\t %dx%d \t \t %.4f \t %.2f \t \t %.4f \t %.2f \t \t %.2f \t \t %.4f \t %.2f\n",i+1,Nx[i],Ny[i],f[i],p[i],f_RE[i],GCI_fine[i],GCI_coarse[i],ratio[i],100*fabs(f[SIZE-1]-f[i])/fabs(f[SIZE-1]));
+			printf("%d.\t %dx%d  \t \t %.4f \t %.2f \t \t %.4f \t %.2f \t \t %.2f \t \t %.4f \t %.2f\n",i+1,Nx[i],Ny[i],f[i],p[i],f_RE[i],GCI_fine[i],GCI_coarse[i],ratio[i],100*fabs(f[SIZE-1]-f[i])/fabs(f[SIZE-1]));
 	}
 	printf("\n\n");
 	printf("Parameter Explanation\n\n");
@@ -79,3 +106,29 @@ int main()
 	printf("\n--- END OF STUDY ---\n\n");
 
 }
+
+
+
+void allocate_int (int*** arr, int row, int col)
+{
+    int i;
+    *arr = (int**)calloc(row , sizeof(int*));
+    for(i=0 ; i<row ; i++)
+        (*arr)[i] = (int*)calloc(col , sizeof(int));
+} 
+
+void allocate_float (float*** arr, int row, int col)
+{
+    int i;
+    *arr = (float**)calloc(row , sizeof(float*));
+    for(i=0 ; i<row ; i++)
+        (*arr)[i] = (float*)calloc(col , sizeof(float));
+} 
+
+void allocate_double (double*** arr, int row, int col)
+{
+    int i;
+    *arr = (double**)calloc(row , sizeof(double*));
+    for(i=0 ; i<row ; i++)
+        (*arr)[i] = (double*)calloc(col , sizeof(double));
+} 
